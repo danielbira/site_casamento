07f59855df19071a625312559e5ca30deb147cb7
alencabira <dbalencastro@gmail.com>
2023-05-04 14:23:29 -0300

    criando contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-console.log("ola mundo")
+AOS.init();
+
+function startTime() {
+    
+    
+    const res = new Date('Oct 22, 2025 16:00:00') - new Date();
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hour = minutes * 60;
+    let day = hour * 24;
+    
+    document.querySelector('.day').innerText = Math.floor(res / day);
+    document.querySelector('.hour').innerText = Math.floor((res % day) / hour);
+    document.querySelector('.minute').innerText = Math.floor((res % hour) / minutes);
+    document.querySelector('.second').innerText = Math.floor((res % minutes) / seconds);
+}
+
+setInterval(startTime, 1000);
+
